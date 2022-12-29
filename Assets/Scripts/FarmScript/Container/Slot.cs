2213849c33a0fae93b90b4b1763a46c973ceda45
PhilippeDum <phil.dum88@gmail.com>
2022-12-29 15:20:17 +0100

    [FIX] Item OnDrop function

--- Assets/Scripts/FarmScript/Container/Slot.cs
+++ Assets/Scripts/FarmScript/Container/Slot.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Slot : MonoBehaviour, IDropHandler
@@ -53,6 +50,51 @@ public class Slot : MonoBehaviour, IDropHandler
         }
 
         if (transform.childCount == 0)
+        {
+            dropped.GetComponent<DraggableItem>().DropItem(transform);
+        }
+    }
+    public void OnDrop2(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag;
+        DraggableItem droppedDItem = dropped.GetComponent<DraggableItem>();
+
+        // If there is already an item
+        if (transform.childCount > 0)
+        {
+            DraggableItem dItemInParent = transform.GetChild(0).GetComponent<DraggableItem>();
+            Item itemInParent = dItemInParent.Item;
+
+            // If 2 items are same && item is stackable && stack size limit is not reached
+            if (itemInParent.Equals(droppedDItem.Item) && itemInParent.isStackable && dItemInParent.QuantityStacked < itemInParent.maxStackSize)
+            {
+                int quantityInDropped = droppedDItem.QuantityStacked;
+                int quantityInDraggable = dItemInParent.QuantityStacked;
+
+                int totalQuantity = quantityInDropped + quantityInDraggable;
+
+                // If total quantity is less or equal than stack size limit
+                if (totalQuantity <= itemInParent.maxStackSize)
+                {
+                    dItemInParent.QuantityStacked = totalQuantity;
+                    dropped.GetComponent<DraggableItem>().DropItem(null, true);
+                }
+
+                // If total quantity is more than stack size limit
+                if (totalQuantity > itemInParent.maxStackSize)
+                {
+                    dItemInParent.QuantityStacked = itemInParent.maxStackSize;
+                    droppedDItem.QuantityStacked = totalQuantity - itemInParent.maxStackSize;
+                    dropped.GetComponent<DraggableItem>().DropItem(null);
+                }
+            }
+            else
+            {
+                dropped.GetComponent<DraggableItem>().DropItem(null);
+            }
+        }
+        // Else it's free
+        else
         {
             dropped.GetComponent<DraggableItem>().DropItem(transform);
         }
