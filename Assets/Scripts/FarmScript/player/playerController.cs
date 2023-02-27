abe531ce15932b35465b3123fcc27ac7548313d7
ludovicmantovani <ludovic.mantov@gmail.com>
2023-02-27 14:55:13 +0100

    [FIX] : Player input

--- Assets/Scripts/FarmScript/player/playerController.cs
+++ Assets/Scripts/FarmScript/player/playerController.cs
@@ -305,4 +305,4 @@ public class playerController : MonoBehaviour
             LoadPlayerPos();
         }
     }
-}}
+}
--- Assets/Scripts/FarmScript/save/List_Slots.cs
+++ Assets/Scripts/FarmScript/save/List_Slots.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 //rajout
 using UnityEngine.InputSystem;
 
