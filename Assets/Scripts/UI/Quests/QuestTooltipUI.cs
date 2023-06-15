b4b69239881b5e82332110e84b2b638f8d8eaab0
ludovicmantovani <ludovic.mantov@gmail.com>
2023-06-15 06:39:59 +0200

    [UPDATE] : hide rewards title in UI if no reward

--- Assets/Scripts/UI/Quests/QuestTooltipUI.cs
+++ Assets/Scripts/UI/Quests/QuestTooltipUI.cs
@@ -13,6 +13,7 @@ namespace Gameplay.UI.Quests
         [SerializeField] private Transform objectiveContainer;
         [SerializeField] private GameObject objectivePrefab;
         [SerializeField] private GameObject objectiveIncompletePrefab;
+        [SerializeField] private GameObject[] rewardUISection;
         [SerializeField] private TextMeshProUGUI rewardText;
         public void Setup(QuestStatus status)
         {
@@ -32,7 +33,21 @@ namespace Gameplay.UI.Quests
                 objectiveText.text = objective.description;
             }
             
-            rewardText.text = GetRewardText(quest);
+            if (rewardUISection != null && quest.GetRewardsCount() > 0)
+            {
+                foreach (GameObject go in rewardUISection)
+                {
+                    go.SetActive(true);
+                }
+                rewardText.text = GetRewardText(quest);
+            }
+            else
+            {
+                foreach (GameObject go in rewardUISection)
+                {
+                    go.SetActive(false);
+                }
+            }
         }
 
         private string GetRewardText(Quest quest)
