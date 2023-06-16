dae28b235e2a0ea744bae3997f3edcd2091052af
PhilippeDum <phil.dum88@gmail.com>
2023-06-16 16:14:09 +0200

    [ADD] Add player position on MapCamera & "Panneau Indicator"

--- Assets/Scripts/FarmScript/MiniMapView.cs
+++ Assets/Scripts/FarmScript/MiniMapView.cs
@@ -14,12 +14,13 @@ public class MiniMapView : MonoBehaviour
     [SerializeField] private RenderTexture renderTexture = null;
     [SerializeField] private string layer;
     [SerializeField] private float spriteScale = 6f;
+    [SerializeField] private SpriteTransformPair playerPosition;
     [SerializeField] private List<SpriteTransformPair> objectsToShow = null;
 
     private Camera _camera = null;
     private AnimalPenManager animalPenManager;
 
-    void Start()
+    private void Start()
     {
         _camera = GetComponent<Camera>();
         animalPenManager = FindObjectOfType<AnimalPenManager>();
@@ -35,46 +36,48 @@ public class MiniMapView : MonoBehaviour
             _camera.targetTexture = renderTexture;
         }
 
+        HandlePicto(playerPosition, false);
+
         foreach (SpriteTransformPair spriteTransformPair in objectsToShow)
         {
             spriteTransformPair.objectTransform = animalPenManager.GetAnimalPenWithPicto(spriteTransformPair.objectName);
 
-            if (spriteTransformPair.objectTransform == null) return;
-
-            GameObject go = new GameObject(spriteTransformPair.objectName, typeof(SpriteRenderer));
-
-            if (layer.Length > 0 && LayerMask.NameToLayer(layer) != -1) go.layer = LayerMask.NameToLayer(layer);
-
-            Renderer objectRenderer = null;
-            spriteTransformPair.objectTransform.gameObject.TryGetComponent<Renderer>(out objectRenderer);
-
-            if (objectRenderer != null)
-            {
-                Vector3 centerPosition = objectRenderer.bounds.center;
-                go.transform.position = new Vector3(
-                    centerPosition.x,
-                    gameObject.transform.position.y - 10f,
-                    centerPosition.z);
-
-            }
-            else
-            {
-                go.transform.position = new Vector3(
-                    spriteTransformPair.objectTransform.position.x,
-                    gameObject.transform.position.y - 10f,
-                    spriteTransformPair.objectTransform.position.z);
-            }
-
-            go.transform.rotation = Quaternion.AngleAxis(-90f, Vector3.right);
-
-            go.transform.localScale = spriteTransformPair.objectTransform.localScale * spriteScale;
-
-            go.GetComponent<SpriteRenderer>().sprite = spriteTransformPair.objectSprite;
+            HandlePicto(spriteTransformPair, true);
         }
     }
 
-    void Update()
+    private void HandlePicto(SpriteTransformPair spriteTransformPair, bool customScale)
     {
+        if (spriteTransformPair.objectTransform == null) return;
 
+        GameObject go = new GameObject(spriteTransformPair.objectName, typeof(SpriteRenderer));
+
+        if (layer.Length > 0 && LayerMask.NameToLayer(layer) != -1) go.layer = LayerMask.NameToLayer(layer);
+
+        Renderer objectRenderer = null;
+        spriteTransformPair.objectTransform.gameObject.TryGetComponent<Renderer>(out objectRenderer);
+
+        if (objectRenderer != null)
+        {
+            Vector3 centerPosition = objectRenderer.bounds.center;
+            go.transform.position = new Vector3(
+                centerPosition.x,
+                gameObject.transform.position.y - 10f,
+                centerPosition.z);
+
+        }
+        else
+        {
+            go.transform.position = new Vector3(
+                spriteTransformPair.objectTransform.position.x,
+                gameObject.transform.position.y - 10f,
+                spriteTransformPair.objectTransform.position.z);
+        }
+
+        go.transform.rotation = Quaternion.AngleAxis(-90f, Vector3.right);
+
+        if (customScale) go.transform.localScale = spriteTransformPair.objectTransform.localScale * spriteScale;
+
+        go.GetComponent<SpriteRenderer>().sprite = spriteTransformPair.objectSprite;
     }
 }
